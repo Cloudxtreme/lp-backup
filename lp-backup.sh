7f0f22c392f39c0f2c7c4990d29d73a8bb85349c
Chris Byerly <cbyerly@gmail.com>
2013-11-20 13:23:26 -0500

    Merge spacecheck function into main script, implemented beginnings of error handling.

--- lp-backup.sh
+++ lp-backup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+set -e
+set -u
 . lp-backup.cfg
-
 
 function DRIVEMOUNT(){
 CHECKMOUNT=$(mount | grep "$DRIVE")
@@ -19,6 +20,39 @@ if [ -z "$CHECKMOUNT" ]; then
  else
  	echo "$DRIVE already mounted."
  fi
+}
+
+function SPACECHECK() {
+DELTRIES=0
+FREEP=$(df -h $DRIVE | awk '{ print $5 }' | sed 's/%//' | tail -1)
+
+if [ "$FREEP" -gt "$FREETHRESH" ]; then
+	echo "There is enough room for a backup run.";
+	#Do backup run function here.
+else
+	#this where the space clean up logic comes in to play
+	#Do cleanup.
+	#Get oldest backup dir based on $DIR and ctime on the directories and nuke it.
+	if [ "$FREEP" -lt "$FREETHRESH" ] && [ "$DELTRIES" -le 2 ]; then
+		while [ "$FREEP" -lt "$FREETHRESH" ] && [ "$DELTRIES" -le 2 ]; do
+			DELDIR=$(/bin/ls -1c $DIR | grep _backup | tail -1)
+			echo "rm -rf $DELDIR"
+			if [ "$FREEP" -gt "$FREETHRESH" ]; then
+				#call backup function here
+				echo "If statement backup run."
+				#break the while loop since the backup can be started.
+				DELTRIES=3
+			else
+				if [ "$DELTRIES" -ge 2 ]; then
+					echo "Can't free space to run backup."
+					exit 1
+				else
+					let DELTRIES=$DELTRIES+1
+				fi
+			fi
+		done
+	fi
+fi
 }
 
 function BACKUP() {
@@ -45,5 +79,6 @@ function BACKUP() {
 		echo "No cPanel user accounts detected. Skipping homedir backup."
 	fi
 }
-
+DRIVEMOUNT
+SPACECHECK
 BACKUP
