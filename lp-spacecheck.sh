74fe78ef3d3a17811b4010451fa725b25ecbbf80
Chris Byerly <cbyerly@gmail.com>
2013-11-20 16:50:40 -0500

    i dont even

--- lp-spacecheck.sh
+++ lp-spacecheck.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 . lp-backup.cfg
 
-DELTRIES=0
+DELTRIES="0"
 function SPACECHECK(){
 	echo "$TS (in SPACECHECK()"
 	FREEP=$(df -h $DRIVE | awk '{ print $5 }' | sed 's/%//' | tail -1)
 	echo "Starting free space percentage: $FREEP"
-	if [ "$FREEP" -ge "$SPACETHRESH" ] #&& [ "$DELTRIES" -le 2 ]; then
+	if [ "$FREEP" -ge "$SPACETHRESH" ] && [ "$DELTRIES" -le 2 ]; then
 		DELDIR=$(/bin/ls -1c $DIR | grep _backup | tail -1)
 		if [ -z "$DELDIR" ]; then
 			echo "Cannot find valid target for removal or deletion tries exceeded."
