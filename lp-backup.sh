68adb10ea08e7f86aae52d88b9a08ad8641b7646
Chris Byerly <cbyerly@gmail.com>
2013-11-21 18:22:58 -0500

    logging redirection for test runs

--- lp-backup.sh
+++ lp-backup.sh
@@ -2,7 +2,10 @@
 #set -e
 set -u
 . lp-backup.cfg
+
 function LOGSTAMP(){
+	#General purpose log time stamping to be used with standard echos,
+	#
 	echo "[$(date +%m-%d-%Y\ %T)]"
 }
 
@@ -14,16 +17,17 @@ function LOGINIT() {
 	else
 		touch $LOG
 	fi
-	#Log cleanup stuff will need to go here.
+	#Log cleanup stuff will need to go here. 
 }
 
 function DRIVEMOUNT(){
 CHECKMOUNT=$(mount | grep "$DRIVE")
 if [ -z "$CHECKMOUNT" ]; then
-	mount $DRIVE $DIR 2&> /dev/null
+	mount $DRIVE $DIR 2>&1 > $LOG
  	CHECKMOUNT=$(mount | grep "$DRIVE")
  	if [ -z "$CHECKMOUNT" ]; then
- 		echo "$LOGSTAMP Could not mount $DRIVE to $DIR." >> $LOG
+ 		echo "$(LOGSTAMP) Could not mount $DRIVE to $DIR; exiting." >> $LOG
+ 		#Alert.
  		exit 1;
  		#Add logging here.a
  	else
@@ -31,31 +35,33 @@ if [ -z "$CHECKMOUNT" ]; then
  		#Add logging here.
  	fi
  else
- 	echo "$LOGSTAMP $DRIVE already mounted."
+ 	echo "$(LOGSTAMP) $DRIVE already mounted."
  fi
 }
 
 function SPACECHECK(){
 	FREEP=$(df -h $DRIVE | awk '{ print $5 }' | sed 's/%//' | tail -1)
-	echo "$LOGSTAMP Starting free space percentage: $FREEP. Thresh is: $FREETHRESH. Deletion attempts: $DELTRIES"
+	echo "$(LOGSTAMP) Free space percentage: $FREEP. Thresh is: $FREETHRESH. \
+		Deletion attempts: $DELTRIES" >> $LOG
 	if [ "$FREEP" -ge "$FREETHRESH" ] && [ "$DELTRIES" -le 2 ]; then
 		DELDIR=$(/bin/ls -1c $DIR | grep _backup | tail -1)
 		if [ -z "$DELDIR" ]; then
-			echo "$LOGSTAMP Cannot find valid target for removal, exiting."
+			echo "$(LOGSTAMP) Cannot find valid target for removal, exiting." >> $LOG
+			#Need cleanup here.
 			exit 1
 		else
-			echo "$LOGSTAMP Removing: $DIR/$DELDIR"
+			echo "$(LOGSTAMP) Removing: $DIR/$DELDIR" >> $LOG
 			/bin/rm -r $DIR/$DELDIR
 			let DELTRIES=$DELTRIES+1
 			SPACECHECK
 		fi
 	else
 		if [ $DELTRIES -gt 2 ] && [ "$FREEP" -ge "$FREETHRESH" ] ; then
-			echo "$LOGSTAMP Deletion attempts exceed, exiting."
+			echo "$(LOGSTAMP) Deletion attempts exceed, exiting." >> $LOG
+			#Need cleanup here.
 			exit 1
 		fi
 	fi
-	echo "Space is adequate, continuing backup."
 }
 
 function BACKUP() {
@@ -66,59 +72,58 @@ function BACKUP() {
 	/bin/mkdir -p $BACKUPDIR
 	#Loop through the defined targets array before moving on to homedirs.
 	for i in "${TARGET[@]}"; do
-		echo "$LOGSTAMP Backing up: $i";
+		echo "$(LOGSTAMP) Backing up: $i" >> $LOG;
 		/usr/bin/rsync -aH --exclude-from 'exclude.txt' $i $BACKUPDIR/
 	done
 	#Get the cPanel users' homedires and back them up to the destination.
 	if $(/bin/ls /var/cpanel/users/ > /dev/null 2>&1); then
-		echo "$LOGSTAMP cPanel users detected. Backing up homedirs."
+		echo "$(LOGSTAMP) cPanel users detected. Backing up homedirs." >> $LOG
 		for i in `/bin/ls /var/cpanel/users`; do
 			VALIDUSER=$(grep $i /etc/passwd | cut -f1 -d:)
 			if [ "$i" == "$VALIDUSER" ]; then
-				echo "$LOGSTAMP Backing up cPanel user: $i";
+				echo "$(LOGSTAMP) Backing up cPanel user: $i" >> $LOG;
 				/usr/bin/rsync -aH $(grep $i /etc/passwd | cut -f6 -d:) $BACKUPDIR; 
 				#Failout if pkgacct fails - need to add a call to UNMOUNT here for cleanup purposes.
 				/usr/local/cpanel/scripts/pkgacct $i $BACKUPDIR/$i --skiphomedir --skipacctdb > /dev/null 2>&1 \
-				|| { echo "$LOGSTAMP Failed packaging cPanel user: $i."; exit 1; };
+				|| { echo "$LOGSTAMP Failed packaging cPanel user: $i." >> $LOG; exit 1; };
 			else
-				echo "$LOGSTAMP Cannot retrieve homedir for user $i. Ignoring."
+				echo "$(LOGSTAMP) Cannot retrieve homedir for user $i. Ignoring." >> $LOG
 			fi
 		done
 	else
-		echo "$LOGSTAMP No cPanel user accounts detected. Skipping homedir backup."
+		echo "$(LOGSTAMP) No cPanel user accounts detected. Skipping homedir backup." >> $LOG
 	fi
 
 	#exit 0
 }
 
 function UNMOUNT(){
-	umount $DIR >> $LOG 2>&1
+	umount $DIR >> $LOG 2>&1 >> $LOG
 	CHECKMOUNT=$(mount | grep "$DRIVE")
 	if [ ! -z "$CHECKMOUNT" ] && [ "$UMOUNTS" -lt 2 ]; then
-		echo "$LOGSTAMP $DRIVE failed to unmount properly, waiting 60 and trying again."
+		echo "$(LOGSTAMP) $DRIVE failed to unmount properly, waiting 60 and trying again." >> $LOG
 		let UMOUNTS=$UMOUNTS+1
-		echo "$LOGSTAMP Unmount attempts: $UMOUNTS"
+		echo "$(LOGSTAMP) Unmount attempts: $UMOUNTS" >> $LOG
 		sleep 2
 		UNMOUNT
 	else
 		if [ ! -z "$CHECKMOUNT" ] && [ "$UMOUNTS" -eq 2 ]; then
-			echo "$LOGSTAMP $DRIVE failed to unmount after three attempts; exiting."
+			echo "$(LOGSTAMP) $DRIVE failed to unmount after three attempts; exiting." >> $LOG
 			exit 1
 		fi
 	fi
 	if [ -z "$CHECKMOUNT" ]; then
-		echo "$LOGSTAMP $DRIVE unmounted successfully."
+		echo "$(LOGSTAMP) $DRIVE unmounted successfully." >> $LOG
 	fi
 }
-echo "$(LOGSTAMP) Beginning backup run."
-sleep 4
-echo "$(LOGSTAMP) Beginning log clean up/initialization:"
+echo "$(LOGSTAMP) Beginning backup run." >> $LOG
+echo "$(LOGSTAMP) Beginning log clean up/initialization:" >> $LOG
 #LOGINIT
-echo "$LOGSTAMP Beginning drive mount:"
+echo "$(LOGSTAMP) Beginning drive mount:" >> $LOG
 #DRIVEMOUNT
-echo "$LOGSTAMP Beginning space check:"
+echo "$(LOGSTAMP) Beginning space check:" >> $LOG
 #SPACECHECK
-echo "$LOGSTAMP Beginning backups:"
+echo "$(LOGSTAMP) Beginning backups:" >> $LOG
 #BACKUP
-echo "$LOGSTAMP Beginning unmount:"
+echo "$(LOGSTAMP) Beginning unmount:" >> $LOG
 #UNMOUNT
